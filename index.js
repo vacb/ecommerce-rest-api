5f5f792112ad9ebaa03f9c9ed9fbf21afe385a13
Victoria Blackburn <v.a.c.blackburn@gmail.com>
2021-05-19 11:12:02 +0100

    Test server with initial GET route and static JSON

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const { response } = require('express');
 const app = express();
 const port = 3000;
 
@@ -9,6 +10,10 @@ app.use(
         extended: true
     })
 )
+
+app.get('/', (req, res) => {
+    res.json({info: 'Node.js, Express, and Postgres API'})
+});
 
 app.listen(port, () => {
     console.log(`App listening on port ${port}`)
